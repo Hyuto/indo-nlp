cc6da233a55aa59553a5719d707adeeeecc41568
Wahyu Setianto <wahyusetianto19@gmail.com>
2022-06-29 23:18:56 +0700

    Prepare v0.1.1

--- indoNLP/__init__.py
+++ indoNLP/__init__.py
@@ -3,6 +3,6 @@
 from indoNLP.preprocessing import *
 
 __all__ = ["preprocessing"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Wahyu Setianto"
 __source__ = "https://github.com/Hyuto/indo-nlp/"
