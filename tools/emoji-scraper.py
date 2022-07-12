5fb80d52a6c965acc1f43f7157279c6ee052fc38
Wahyu Setianto <wahyusetianto19@gmail.com>
2022-07-13 04:23:26 +0700

    Finishing scraping emoji scripts

--- tools/emoji-scraper.py
+++ tools/emoji-scraper.py
@@ -1,25 +1,45 @@
-import json
-import re
-import unicodedata
+import logging
 import requests
 from bs4 import BeautifulSoup
 
+from utils import format_unicode, normalizer, write_python_file
 
-def normalizer(text: str) -> str:
-    """Unicode text normalizer
-
-    Args:
-        text (str): raw text
-
-    Returns:
-        str: cleaned text
-    """
-    text = unicodedata.normalize("NFKD", text.lower()).encode("ascii", "ignore").decode("ascii")
-    text = re.sub(":", "", text)
-    return re.sub(" ", "_", text.strip())
+# Setup logging
+logging.basicConfig(
+    format="[%(levelname)s] %(asctime)s %(filename)s -- %(message)s",
+    level=logging.INFO,
+)
 
 
-def get_emoji_data(url: str) -> dict:
+def get_emoji_data_unicode(url):
+    def _parse_line(line):
+        code = format_unicode(line.split(";")[0].strip())
+        status = line.split(";")[1].split("#")[0].strip()
+        title = normalizer(" ".join(line.split(";")[1].split("#")[1].split()[2:]))
+        return code, status, title
+
+    status_mapper = {
+        "component": 1,
+        "fully-qualified": 2,
+        "minimally-qualified": 3,
+        "unqualified": 4,
+    }
+
+    page = requests.get(url).text.split("\n")
+    data = {}
+    for line in page:
+        if line != "":
+            if line[0] != "#":
+                code, status, title = _parse_line(line)
+                if title not in data:
+                    data[title] = {"code": code, "status": status_mapper[status]}
+                else:
+                    if status_mapper[status] < data[title]["status"]:
+                        data[title] = {"code": code, "status": status_mapper[status]}
+    return data
+
+
+def get_emoji_data_emojiterra(url: str) -> dict:
     """Get emoji data from https://emojiterra.com
 
     Args:
@@ -45,22 +65,33 @@ def get_emoji_data(url: str) -> dict:
 
 
 def main():
-    emoji_data_en = get_emoji_data("https://emojiterra.com/copypaste/")
-    emoji_data_id = get_emoji_data("https://emojiterra.com/copypaste/id/")
+    emoji_dict = get_emoji_data_unicode("https://unicode.org/Public/emoji/14.0/emoji-test.txt")
+    emoji_data_en = get_emoji_data_emojiterra("https://emojiterra.com/copypaste/")
+    emoji_data_id = get_emoji_data_emojiterra("https://emojiterra.com/copypaste/id/")
 
     assert len(emoji_data_en) >= len(emoji_data_id), "id emoji > en emoji"
 
     result = {}
     for x in emoji_data_en:
-        result[x] = {"en": emoji_data_en[x]["title"], "alias": {"en": emoji_data_en[x]["alias"]}}
-        if x in emoji_data_id.keys():
-            result[x]["id"] = emoji_data_id[x]["title"]
-            result[x]["alias"]["id"] = emoji_data_id[x]["alias"]
-        else:
-            print(f"missing indonesian emoji : {x} - {emoji_data_en[x]['title']}")
+        title = emoji_data_en[x]["title"]
 
-    with open("emoji-data.json", "w") as reader:
-        reader.write(json.dumps(result, indent=4))
+        if title in emoji_dict:
+            code = emoji_dict[title]["code"]
+        else:
+            code = x
+            logging.warning(f"not found code emoji : {emoji_data_en[x]['title']}")
+
+        result[code] = {"en": emoji_data_en[x]["title"]}
+
+        if x in emoji_data_id.keys():
+            result[code]["id"] = emoji_data_id[x]["title"]
+
+            if emoji_data_id[x]["title"] != emoji_data_id[x]["alias"]:
+                result[code]["alias"] = emoji_data_id[x]["alias"]
+        else:
+            logging.warning(f"missing indonesian emoji : {x} - {emoji_data_en[x]['title']}")
+
+    write_python_file(result)
 
 
 if __name__ == "__main__":
---
+++ tools/utils.py
@@ -0,0 +1,49 @@
+import json
+import re
+import unicodedata
+
+
+def normalizer(text: str) -> str:
+    """Unicode text normalizer
+
+    Args:
+        text (str): raw text
+
+    Returns:
+        str: cleaned text
+    """
+    text = unicodedata.normalize("NFKD", text.lower()).encode("ascii", "ignore").decode("ascii")
+    text = re.sub("[:,]", "", text)
+    return re.sub(" ", "_", text.strip())
+
+
+def format_unicode(text: str) -> str:
+    """Format unicode string
+
+    Args:
+        text (str): unicode string
+
+    Returns:
+        str: formated unicode string
+    """
+    return "".join(
+        ["\\U0000" + code if len(code) == 4 else "\\U000" + code for code in text.split()]
+    )
+
+
+def write_python_file(emoji_data: dict) -> None:
+    """Write emoji data to python file
+
+    Args:
+        emoji_data (dict): emoji data dictionary
+    """
+    template = "# -*- coding: utf-8 -*-\n\n"
+    template += '"""Data containing emoji mapper retrieved from https://emojiterra.com\n'
+    template += '   and https://unicode.org/Public/emoji/14.0/emoji-test.txt"""\n\n'
+    template += '__all__ = ["EMOJI_DATA"]\n\n'
+    template += "EMOJI_DATA = {\n"
+    template += ",\n".join([f'    u"{x}" : {json.dumps(emoji_data[x])}' for x in emoji_data])
+    template += "\n}"
+
+    with open("emoji_data.py", "w") as writer:
+        writer.write(template)
