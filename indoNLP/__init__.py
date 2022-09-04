5f2cd47182c15a9a5d79197dfed0582dd420f5a6
Wahyu Setianto <wahyusetianto19@gmail.com>
2022-09-04 22:36:58 +0700

    Release v0.3.2

--- indoNLP/__init__.py
+++ indoNLP/__init__.py
@@ -4,6 +4,6 @@ import indoNLP.dataset
 import indoNLP.preprocessing
 
 __all__ = ["dataset", "preprocessing"]
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Wahyu Setianto"
 __source__ = "https://github.com/Hyuto/indo-nlp/"
